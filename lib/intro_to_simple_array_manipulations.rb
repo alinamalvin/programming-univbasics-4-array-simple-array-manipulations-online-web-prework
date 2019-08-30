dedd27728f0f478c724b1b3bc5bc218e5771236a
Alina Kornilova <alinakornilova78@gmail.com>
2019-08-30 03:24:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,4 +23,7 @@ def shift_with_args(ice_cream_brands)
 end
 
 def using_concat()
-
+ @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["sports cars", "flatiron school"]
+    @all_my_favs = using_concat(@my_favorite_things, more_favs)
+  end
